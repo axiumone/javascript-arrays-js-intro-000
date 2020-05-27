6b2bac9047438d2bf4fb2a70874309fafd7580dc
Dmitry Sychev <axiumone@gmail.com>
2020-05-27 01:27:39 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -20,6 +20,7 @@ function destructivelyAddElementToEndOfArray(chocolateBars, chips){
   chocolateBars.push(chips);
   return chocolateBars;
 }
+<<<<<<< HEAD
 
 function accessElementInArray(chocolateBars, index){
   return chocolateBars[index];
@@ -42,3 +43,5 @@ function destructivelyRemoveElementFromEndOfArray(chocolateBars){
 function removeElementFromEndOfArray(chocolateBars){
     return chocolateBars.slice(0, chocolateBars.length - 1);
 }
+=======
+>>>>>>> c152ab1856616376c9175d8a21422c30e9b4b987
